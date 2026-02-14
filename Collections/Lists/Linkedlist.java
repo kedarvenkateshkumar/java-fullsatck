33ed68c5484bb12402c71c68f146f54fc04fc70b
kedarvenkateshkumar <venkatesh.veeramallu@sasi.ac.in>
2026-02-14 20:43:00 +0530

    Full design of linkedlist

--- Collections/Lists/Linkedlist.java
+++ Collections/Lists/Linkedlist.java
@@ -24,14 +24,13 @@ class ListNode{
             Node newNode = new Node(data);
 
             while(head.next != null){
-
                 head = head.next;
             }
             head.next = newNode;
         }
         //delete a data from list
         public void delete(int data){
-            
+
             if(head == null) return;
             if(head.data == data) head = head.next;
 
